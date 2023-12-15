693c76b19ad08f896ebe444bf6dcdc2a51122ae5
Akn <sibata486@gmail.com>
2023-12-15 22:19:15 +0900

    init /

---
+++ BinReader_Console/Models/ByteWrapper.cs
@@ -0,0 +1,6 @@
+namespace BinReader_Console.Models
+{
+    public struct ByteWrapper
+    {
+    }
+}
---
+++ BinReader_ConsoleTests/Models/ByteWrapperTest.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+
+namespace BinReader_ConsoleTests.Models
+{
+    [TestFixture]
+    public class ByteWrapperTest
+    {
+        [Test]
+        public void Test()
+        {
+        }
+    }
+}
