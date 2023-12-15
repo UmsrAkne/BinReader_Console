520418d4fa471d841f10af77730f911bac02f6e9
Akn <sibata486@gmail.com>
2023-12-15 23:42:38 +0900

    add / byte を比較・抽出するクラスの実装

--- BinReader_Console/Models/ByteWrapper.cs
+++ BinReader_Console/Models/ByteWrapper.cs
@@ -2,5 +2,18 @@ namespace BinReader_Console.Models
 {
     public struct ByteWrapper
     {
+        public byte Value { get; set; }
+
+        public bool IsWildcard { get; set; }
+
+        public bool AreEqual(byte b)
+        {
+            if (IsWildcard)
+            {
+                return true;
+            }
+
+            return b == Value;
+        }
     }
 }
---
+++ BinReader_ConsoleTests/Models/ComparerTest.cs
@@ -0,0 +1,54 @@
+using BinReader_Console.Models;
+using NUnit.Framework;
+
+namespace BinReader_ConsoleTests.Models
+{
+    public class ComparerTest
+    {
+        [Test]
+        [TestCase(new byte[] { 0x00, 0x01, 0x02, 0x03, 0x00, 0x01, 0x02, 0x01, 0x00, 0x01, 0x00, },
+            new byte[] { 0x02, 0x03, 0x02, 0x01, 0x00, })]
+        [TestCase(new byte[] { 0x00, 0x01, 0x02, 0x03, 0x00, 0x00, 0x02, 0x01, 0x00, 0x01, 0x00, },
+            new byte[] { 0x02, 0x03, 0x00, 0x00, 0x02, 0x01, 0x00, })]
+        public void GetMatchedBytesTest(byte[] bytes, byte[] result)
+        {
+            var wrappers = new ByteWrapper[]
+            {
+                new () { Value = 0x0, },
+                new () { Value = 0x1, },
+            };
+
+            var bs = Comparer.GetMatchedBytes(bytes, wrappers);
+            CollectionAssert.AreEqual(result, bs);
+        }
+
+        [Test]
+        public void GetMatchedBytesTest_ワイルドカード()
+        {
+            var bytes = new byte[] { 0x00, 0x02, 0x02, 0x03, 0x00, 0x03, };
+            var wrappers = new ByteWrapper[]
+            {
+                new () { Value = 0x0, },
+                new () { Value = 0x0, IsWildcard = true, },
+            };
+
+            var bs = Comparer.GetMatchedBytes(bytes, wrappers);
+            CollectionAssert.AreEqual(new byte[] { 0x02, 0x03, }, bs);
+        }
+
+        [Test]
+        public void GetMatchedBytesTest_ワイルドカード2()
+        {
+            var bytes = new byte[] { 0x01, 0x02, 0x00, 0x03, 0x00, 0x03, };
+            var wrappers = new ByteWrapper[]
+            {
+                new () { Value = 0x0, },
+                new () { Value = 0x0, IsWildcard = true, },
+                new () { Value = 0x0, },
+            };
+
+            var bs = Comparer.GetMatchedBytes(bytes, wrappers);
+            CollectionAssert.AreEqual(new byte[] { 0x03, }, bs);
+        }
+    }
+}
